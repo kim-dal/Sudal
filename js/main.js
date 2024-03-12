d51038940671c48896ac99b7a7df90bbc5f588d5
Kim dal <qazsw@naver.com>
2024-03-13 00:28:15 +0900

    키비쥬얼 디자인 수정 및 스크립트 수정

--- js/main.js
+++ js/main.js
@@ -1,49 +1,55 @@
-$(function () {
-  const introSection = $("#load");
-  const textMove = $("#load .textWrap");
 
-  setTimeout(function () {
-    textMove.addClass("on");
-  }, 1000);
+$(document).ready(function() {
+  $(function () {
+    const introSection = $("#load");
+    const textMove = $("#load .textroll");
 
-  setTimeout(function () {
-    introSection.removeClass("on");
-  }, 3000);
-});
+    setTimeout(function () {
+      textMove.addClass("on");
+    }, 1000);
 
-$(".goTop").on("click", function () {
-  scroll.scrollTo(slider);
-});
-$(function () {
-  $(".goTop").on("click", function () {
-    $("html, body").animate({ scrollTop: 0 }, 400);
+    setTimeout(function () {
+      introSection.removeClass("on");
+    }, 3000);
   });
-});
+  
+  let $header = $('#header');
+  let $section = $('#section1');
+  let changeY = $section.outerHeight();
+  let positionY = $(document).scrollTop();
 
-$(function () {
-  let locoScroll;
-  const $header = $("#header");
-  const $page = $(".section2");
-  let pageOffsetTop = $page.offset().top;
-
-  locoScroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    smoothMobile: true,
-  });
-
-  new ResizeObserver(() => locoScroll.update()).observe(
-    document.querySelector("[data-scroll-container]")
-  );
-
-  locoScroll.on("scroll", (instance) => {
-    if (instance.scroll.y > pageOffsetTop) {
+  function headerState(positionY){
+    if ( positionY > changeY ) {
       $header.addClass("bk");
     } else {
       $header.removeClass("bk");
     }
+    console.log(changeY);
+  };
+
+  $(window).on('scroll', function(){
+    positionY = $(document).scrollTop();
+    headerState(positionY);
   });
-  locoScroll.on("resize", (instance) => {
-    pageOffsetTop = $page.offset().top;
+    
+  $(window).on('resize', function(){
+    positionY = $(document).scrollTop();
+    headerState(positionY);
   });
+
+  const content = "EXCELLENT";
+  const text = document.querySelector(".text");
+  let i = 0;
+
+  function typing(){
+      let txt = content[i++];
+      text.innerHTML += txt=== "\n" ? "<br/>": txt;
+      if (i > content.length) {
+          text.textContent = "";
+          i = 0;
+      }
+  }
+  setInterval(typing, 300)
+
+  AOS.init();
 });
