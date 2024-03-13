71fe2b52c08c7d1209a29ad23cced2d004c7a6cc
suyeonkim00 <suyeon.kim@dotzlab.co.kr>
2024-03-13 16:50:00 +0900

    제이쿼리 > 스크립트 수정

--- js/main.js
+++ js/main.js
@@ -1,55 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const introSection = document.getElementById("load");
+  const introText = document.querySelector("#load .textroll");
 
-$(document).ready(function() {
-  $(function () {
-    const introSection = $("#load");
-    const textMove = $("#load .textroll");
+  setTimeout(function () {
+    introText.classList.add("on");
+  }, 1000);
 
-    setTimeout(function () {
-      textMove.addClass("on");
-    }, 1000);
+  setTimeout(function () {
+    introSection.classList.remove("on");
+  }, 3000);
 
-    setTimeout(function () {
-      introSection.removeClass("on");
-    }, 3000);
-  });
-  
-  let $header = $('#header');
-  let $section = $('#section1');
-  let changeY = $section.outerHeight();
-  let positionY = $(document).scrollTop();
-
-  function headerState(positionY){
-    if ( positionY > changeY ) {
-      $header.addClass("bk");
-    } else {
-      $header.removeClass("bk");
-    }
-    console.log(changeY);
-  };
-
-  $(window).on('scroll', function(){
-    positionY = $(document).scrollTop();
-    headerState(positionY);
-  });
-    
-  $(window).on('resize', function(){
-    positionY = $(document).scrollTop();
-    headerState(positionY);
-  });
-
-  const content = "EXCELLENT";
-  const text = document.querySelector(".text");
+  const typingText = "EXCELLENT";
+  const typingClass = document.querySelector(".text");
   let i = 0;
 
-  function typing(){
-      let txt = content[i++];
-      text.innerHTML += txt=== "\n" ? "<br/>": txt;
-      if (i > content.length) {
-          text.textContent = "";
-          i = 0;
-      }
-  }
-  setInterval(typing, 300)
+  let typing = () => {
+    let txt = typingText[i++];
+    typingClass.innerHTML += txt;
+    if (i > typingText.length) {
+      typingClass.textContent = "";
+      i = 0;
+    }
+  };
+  setInterval(typing, 300);
 
-  AOS.init();
+  const header = document.getElementById("header");
+  const section = document.getElementById("section1");
+  let changeY = section.offsetHeight;
+  let positionY = window.scrollY;
+
+  let headerState = (positionY, changeY) => {
+    if (positionY > changeY) {
+      header.classList.add("bk");
+    } else {
+      header.classList.remove("bk");
+    }
+  };
+
+  document.addEventListener("scroll", () => {
+    positionY = window.scrollY;
+    changeY = section.offsetHeight;
+    headerState(positionY, changeY);
+  });
+
+  document.addEventListener("resize", () => {
+    positionY = window.scrollY;
+    changeY = section.offsetHeight;
+    headerState(positionY, changeY);
+  });
 });
